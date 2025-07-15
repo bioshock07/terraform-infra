0fd0b8017d144cc22c4b7446f99d314ae82eecde
chaitanya <chaitanyak97@gmail.com>
2025-07-15 16:31:40 +0530

    tfv update

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-//resource "aws_s3_bucket" "example" {
-//  bucket = var.bucket_name
-//}
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+}
 
-//resource "aws_s3_bucket_acl" "example_acl" {
-//  bucket = aws_s3_bucket.example.id
-//  acl    = "private"
-//}
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
 
 terraform {
   backend "s3" {
