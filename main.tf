e1ab94750331102c9f56b1dcf0e527133a55ae50
chaitanya <chaitanyak97@gmail.com>
2025-07-15 16:14:15 +0530

    maintf update

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
-}
+//resource "aws_s3_bucket" "example" {
+//  bucket = var.bucket_name
+//}
 
-resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
+//resource "aws_s3_bucket_acl" "example_acl" {
+//  bucket = aws_s3_bucket.example.id
+//  acl    = "private"
+//}
+
+terraform {
+  backend "s3" {
+    bucket         = "jenkins-tfbuck"
+    key            = "jenkins/infra.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 }
